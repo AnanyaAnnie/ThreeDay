48c803c255401aed59905a2a96e5d083aa598e59
Ananya <122010976+AnanyaAnnie@users.noreply.github.com>
2023-02-12 19:23:47 +0530

    Add files via upload

---
+++ CountTheNumb.java
@@ -0,0 +1,15 @@
+public class CountTheNumb {
+    public static void main(String[] args) {
+        String str = "welcome to the world of java";
+
+        int count = 1;
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if ((str.charAt(i) == ' ') && (str.charAt(i + 1) != ' ')) {
+                count++;
+            }
+        }
+        System.out.println("Number of words in a string : " + count);
+    }
+    
+}
